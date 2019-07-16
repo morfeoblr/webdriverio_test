1f9a2007eaff01966c0085580cd4d781209a0ea2
Aliaksei Bakhur <morfeo.blr@gmail.com>
2019-07-17 00:35:44 +0300

    First test and config changes

--- test/steps/test.js
+++ test/steps/test.js
@@ -1,14 +1,23 @@
-/* global browser,expect,assert */
+/* global $, browser,expect,assert */
 const { defineSupportCode } = require('cucumber');
 const assert = require('assert');
 
 defineSupportCode(({ Given, Then, When }) => {
   Given(/^I open the base url$/, () => {
-    browser.url('https://webdriver.io');
+    browser.url('https://www.skyscanner.net/');
   });
 
   Given(/^I open the base url2$/, () => {
     const title = browser.getTitle();
-    assert.strictEqual(title, 'WebdriverIO · Next-gen WebDriver test framework for Node.js');
+    const locales = $('div[class*="CultureSelectorButton"]');
+    locales.click();
+    // $('div[class*="bpk-modal__content"]').waitForDisplayed();
+    const culture = $('#culture-selector-locale');
+    culture.click();
+    // $('option[value="en-US"]').waitForDisplayed();
+    $('option[value="en-US"]').click();
+    $('#culture-selector-save').click();
+    const selectedLocale = locales.getText();
+    assert.strictEqual(selectedLocale, 'English (US)');
   });
 });
